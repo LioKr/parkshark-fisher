f5ca821b73d007e45a38cacf7fb8f6f9fea8c45a
LioKr <lio.krebs@gmail.com>
2021-04-19 17:27:25 +0200

    setup | creating2

---
+++ src/main/java/com/switchfully/parksharkfisher/ParksharkFisherApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.parksharkfisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksharkFisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParksharkFisherApplication.class, args);
+    }
+
+}
