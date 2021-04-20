1cc6d1f19e17f088aa78d49517cc63e413d49b8c
bchaerel <brecht.chaerel@gmail.com>
2021-04-20 13:45:38 +0200

    DI1 database tables added

---
+++ src/main/java/com/switchfully/parksharkfisher/domain/dbscripts/createTable.sql
@@ -0,0 +1,11 @@
+drop table divisions if exists
+create table divisions
+(
+    id varchar(60) not null
+        constraint divisions_pk
+            primary key,
+    division_name varchar(60) not null,
+    original_name varchar(60),
+    director varchar(60) not null
+);
+
