602c6a60c8be11a848e065033850591df99f3b3f
LioKr <lio.krebs@gmail.com>
2021-04-22 14:02:49 +0200

    ME1 unit tests

--- src/test/java/com/switchfully/parksharkfisher/service/MemberServiceImplementationTest.java
+++ src/test/java/com/switchfully/parksharkfisher/service/MemberServiceImplementationTest.java
@@ -1,9 +1,27 @@
 package com.switchfully.parksharkfisher.service;
 
+import com.switchfully.parksharkfisher.domain.repositories.MemberRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceImplementationTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+    @InjectMocks
+    private MemberServiceImplementation memberServiceImplementation;
+
+    @Test
+    void createMember_givenCorrectParameter_thenVerifyTheConnectionOfServiceAndMapper() {
+        memberServiceImplementation.save(any());
+        Mockito.verify(memberRepository).save(any());
+    }
 
 }
