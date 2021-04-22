82c9bef0b66deda70a1ece7482d6388c704b39de
bchaerel <brecht.chaerel@gmail.com>
2021-04-22 16:20:05 +0200

    ML1: added member unit tests

---
+++ src/test/java/com/switchfully/parksharkfisher/domain/entities/MemberTest.java
@@ -0,0 +1,60 @@
+package com.switchfully.parksharkfisher.domain.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberTest {
+
+    @Test
+    void whenCreatingANewMemberWithBronzeMemberShip_thenAssertMembershipIsActuallyBronze() {
+        //when
+        Member newMember = new Member("first",
+                "last",
+                "0012345678",
+                "mail@mail.com",
+                new Address("Street", "2", "3000", "Leuven"),
+                new LicensePlate("plate", "country"),
+                LocalDate.now(),
+                Membership.BRONZE);
+
+        //then
+        Assertions.assertEquals(Membership.BRONZE, newMember.getMembership());
+    }
+
+    @Test
+    void whenCreatingANewMemberWithGoldMemberShip_thenAssertMembershipIsActuallyGold() {
+        //when
+        Member newMember = new Member("first",
+                "last",
+                "0012345678",
+                "mail@mail.com",
+                new Address("Street", "2", "3000", "Leuven"),
+                new LicensePlate("plate", "country"),
+                LocalDate.now(),
+                Membership.GOLD);
+
+        //then
+        Assertions.assertEquals(Membership.GOLD, newMember.getMembership());
+    }
+
+    @Test
+    void whenCreatingANewMemberWithNoMemberShip_thenAssertMembershipIsBronze() {
+        //when
+        Member newMember = new Member("first",
+                "last",
+                "0012345678",
+                "mail@mail.com",
+                new Address("Street", "2", "3000", "Leuven"),
+                new LicensePlate("plate", "country"),
+                LocalDate.now(),
+                null);
+
+        //then
+        Assertions.assertEquals(Membership.BRONZE, newMember.getMembership());
+    }
+
+}
